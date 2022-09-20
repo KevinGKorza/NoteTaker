1ae81f9c397a916b78ba8fb4b979310289da5849
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-20 13:56:29 -0400

    Delete uuid.js

--- uuid.js
+++
@@ -1,9 +0,0 @@
-//Express
-//Immediately export a function that generates a string of random numbers and letters
-//Helper
-module.exports = () =>
-Math.floor((1 + Math.random()) * 0x10000)
-.toString(16)
-.substring(1);
-
-
