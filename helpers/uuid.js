3d47f81469dd4c36225cff238ce745c877dd36bb
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-09-20 13:56:51 -0400

    Update uuid.js

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,1 +1,9 @@
+//Express
+//Immediately export a function that generates a string of random numbers and letters
+//Helper
+module.exports = () =>
+Math.floor((1 + Math.random()) * 0x10000)
+.toString(16)
+.substring(1);
+
 
